6927b262129dba875bbe8e238334830eb5b8b153
Raj Saini <98858458+Rajmaterbot@users.noreply.github.com>
2024-05-30 13:57:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 28308147
+api_id = "28308147"
 
 api_hash = "8240cdc945560ccd108b6ff2925ced5e"
 
-bot_token = "7168874347:AAHV4ukO1257n6wiUOL2b73ZQsoeiIzWLNg"
+bot_token = "7085871742:AAFnhC3dVkvmr1JyVSfP3u4EtswiQ9wsTP4"
 
